1f9da47b23384971d27badf6d8195981c836b946
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2023-10-09 14:59:34 +0100

    fix: trigger deployment

--- server/app.spec.js
+++ server/app.spec.js
@@ -8,7 +8,7 @@ describe('', () => {
     expect(res.statusCode).eq(200);
   });
 
-  it('Test header: content-type: application/json', async () => {
+  it('Test header: content-type:  application/json', async () => {
     const res = await request(server).post('/');
     expect(res.statusCode).eq(415);
     expect(res.body)
